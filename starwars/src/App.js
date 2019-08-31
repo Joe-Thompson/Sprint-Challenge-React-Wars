1621f4369ca838c2a2e38b552bb20fa893ce387d
Joe Thompson <joe@pop-os.localdomain>
2019-08-30 21:58:11 -0400

    Api data rendered and basic styling done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import CharacterCard from "./components/CharacterCard";
+import Header from "./components/Header";
 
 const App = () => {
+
+  const [person, setPerson] = useState([]);
+
+  useEffect(() => {
+      axios
+          .get('https://swapi.co/api/people')
+          .then(res => setPerson(res.data.results))
+          .catch(err => console.log(err))
+  },[]);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +24,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Header/>
+        {person.map((item, index) => {
+            return (
+                <CharacterCard index={index} key={index} data={item} />
+            )
+        })}
     </div>
   );
 }
